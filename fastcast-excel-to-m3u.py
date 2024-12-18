5833417687fc59564a06e5411373d336a23261d7
lee <39603399+leecstevens@users.noreply.github.com>
2024-12-18 16:26:47 -0600

    0.6: Cosmetic and cleanup

--- fastcast-excel-to-m3u.py
+++ fastcast-excel-to-m3u.py
@@ -3,8 +3,7 @@ Fastcast Excel Export to M3U conversion tool.
 Written By: Lee Stevens
 """
 import os
-import openpyxl
-import openpyxl.workbook #For reading Excel files in Python
+import openpyxl #For reading Excel files in Python
 
 def find_excel():
     # Grab all the xlsx files and return dictionary for processing
@@ -24,22 +23,29 @@ def process_excel_files(file_list):
         playlist_items = list(sheet.values)[1:]
         for item in playlist_items:
             playlist_name = file.split('.xls')[0]
+            output_file = '%s.m3u' % (playlist_name)
             file_path = item[1]
             artist = item[2]
             track = item[3]
             length = time_to_seconds(item[4]) # Convert time to integer seconds
+            
             m3u_data.append("EXTINF:%s,%s - %s\n%s" % (
                     length,
                     artist,
                     track,
                     file_path
-                )
+                ) # Accomodate the file format of M3U
             )
-        with open('%s.m3u' % (playlist_name),'w') as m3u_file: # This will overwrite if it exists.
-            m3u_file.write('\n'.join(m3u_data))
-            m3u_file.close()
+        try:
+            with open(output_file,'w') as m3u_file: # This will overwrite if it exists.
+                m3u_file.write('\n'.join(m3u_data))
+                m3u_file.close()
+                print('Output File Created: %s' % (output_file))
+        except Exception as e:
+            print('File %s not created.\nReason: %s' % (output_file, e))
 
 def time_to_seconds(in_time):
+    # This is needed in the M3U format file
     time_split = in_time.split(':')
     
     seconds = int(time_split[-1]) # Add just the seconds
@@ -52,5 +58,5 @@ def time_to_seconds(in_time):
 
 def main():
     process_excel_files(find_excel())
-
+    
 main()
