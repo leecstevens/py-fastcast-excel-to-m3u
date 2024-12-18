94a7d6d35a564c8b6476ad377350285afa720c57
lee <39603399+leecstevens@users.noreply.github.com>
2024-12-18 14:34:14 -0600

    0.3: Read in excel files

--- fastcast-excel-to-m3u.py
+++ fastcast-excel-to-m3u.py
@@ -3,6 +3,8 @@ Fastcast Excel Export to M3U conversion tool.
 Written By: Lee Stevens
 """
 import os
+import openpyxl
+import openpyxl.workbook #For reading Excel files in Python
 
 def find_excel():
     # Grab all the xlsx files and return dictionary for processing
@@ -12,7 +14,43 @@ def find_excel():
             excel_files.append(file)
     return excel_files
 
+def process_excel_files(file_list):
+    #Variables more verbose for readability
+    for file in file_list:
+        print('Processing File: %s' % (file))
+        workbook = openpyxl.load_workbook(file)
+        sheet = workbook.worksheets[0] # Only need Sheet1
+        playlist_items = list(sheet.values)[1:]
+        playlist = []
+        for item in playlist_items:
+            order = item[0]
+            file_path = item[1]
+            artist = item[2]
+            track = item[3]
+            length = time_to_seconds(item[4]) # Convert time to integer seconds
+            playlist.append(
+                (
+                    order,
+                    file_path,
+                    artist,
+                    track,
+                    length
+                ) # Create a tuple inside the list
+            )
+        print(playlist)
+
+def time_to_seconds(in_time):
+    time_split = in_time.split(':')
+    
+    seconds = int(time_split[-1]) # Add just the seconds
+    seconds += int(time_split[-2]) * 60 # Add 60 seconnds per minute
+    if len(time_split) == 3:
+        seconds += int(time_split[-3]) * 3600 # In case track is > 1 hour, add 3600 seconds per hour
+
+    return seconds
+
+
 def main():
-    import_files = find_excel()
+    process_excel_files(find_excel())
 
 main()
